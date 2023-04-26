06b956c9a6a7f43d0166d8eb6fcb5c9b5f9f313c
Ramón Márquez <ramon.marquez@resurfacelabs.com>
2023-04-26 11:57:25 -0400

    feat: add support for debug export and verbosity

--- buildx/scripts/sniffer.sh
+++ buildx/scripts/sniffer.sh
@@ -4,19 +4,21 @@ device=${NET_DEVICE:-$VPC_MIRROR_DEVICE}
 ports=${APP_PORTS:-$APP_PORT}
 mode=$SNIFFER_ENGINE
 debug=$SNIFFER_DEBUG_ENABLED
+verbosity=${SNIFFER_DEBUG_VERBOSITY:=0}
+export_to=${SNIFFER_DEBUG_EXPORT:=none}
 
-bpf() {    
-    bpf_ports=$(echo $1 | sed 's/,/ or /g')
-    echo "(dst port ($bpf_ports)) or (src port ($bpf_ports))"
+bpf() {
+  bpf_ports=$(echo $1 | sed 's/,/ or /g')
+  echo "(dst port ($bpf_ports)) or (src port ($bpf_ports))"
 }
 
 rules() {
-    echo "$(echo -e $USAGE_LOGGERS_RULES)"
+  echo "$(echo -e $USAGE_LOGGERS_RULES)"
 }
 
 inputs() {
   options="--input-raw $device:$ports"
-  
+
   if [[ "$mode" == "mirror" ]]; then
     options="${options} --input-raw-engine vxlan"
     options="${options} --input-raw-vxlan-port ${SNIFFER_MIRROR_VXLANPORT:-4789}"
@@ -35,23 +37,45 @@ inputs() {
   #  "--input-raw-ignore-interface"
   #  "--input-raw-k8s-skip-ns"
   #  "--input-raw-k8s-skip-svc"
-  elif [[ "$mode" ==  "k8s" ]]; then
-    options=$K8S_INPUT
+  elif [[ "$mode" == "k8s" ]]; then
+    # options=$K8S_INPUT
+    options="${K8S_INPUT} --input-raw-track-response"
   else
     options="${options} --input-raw-track-response"
     options="${options} --input-raw-bpf-filter "
   fi
-  
+
   echo $options
 }
 
 outputs() {
   options="--output-resurface $USAGE_LOGGERS_URL"
-  
+
   if [[ "$debug" == "true" ]]; then
-    options="${options} --output-stdout --http-pprof :8181"
+    if [ "$verbosity" -gt 0 ]; then
+      options="${options} --verbose ${verbosity}"
+    fi
+    if [ "$verbosity" -ge 1 ]; then
+      options="${options} --http-pprof :8181"
+    fi
+    if [ "$verbosity" -ge 2 ]; then
+      options="${options} --output-stdout"
+    fi
+
+    if [ "${export_to}" != "none" ]; then
+      if [ "${export_to}" == "s3" ]; then
+        options="${options} --output-file s3://sniffer-debug-logs/${SNIFFER_DEBUG_DEAL_ID}/%Y-%m-%d.gz"
+      elif [ "${export_to}" == "local" ]; then
+        options="${options} --output-file ./%Y-%m-%d.gz"
+      elif [ "${export_to}" == "http" ]; then
+        options="${options} --output-http ${SNIFFER_DEBUG_HTTP_ENDPOINT}"
+      else
+        echo 'Invalid export option. Supported values are "s3", "local", "http", or "none"'
+        exit 1
+      fi
+    fi
   fi
-  
+
   options="${options} --output-resurface-rules "
 
   echo $options
